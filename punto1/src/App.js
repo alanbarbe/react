90a60805f6230a991e18184d199662d5e49d6595
Alan Barbe <alanbarbecabj@gmail.com>
2024-08-27 00:10:23 -0300

    upd

---
+++ punto1/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+const HolaMundo = () => {
+    return (
+        <div className="hola-mundo-container">
+            <h1 className="hola-mundo-texto">¡Hola, mundo!</h1>
+        </div>
+    );
+};
+
+export default HolaMundo;
+
